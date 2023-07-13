4d5033393e4717f74d920cd37a65021ade2742a6
EC2 Default User <ec2-user@ip-172-31-43-94.ap-northeast-1.compute.internal>
2023-07-13 08:03:56 +0000

    マイページ編集機能作成中

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,5 +4,19 @@ class UserController < ApplicationController
   end
 
   def edit
+    @user = Customer.find(params[:id])
   end
 end
+
+  def update
+    user = Customer.find(params[:id])
+    user.update(customer_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(current_customer.id) #ユーザーの投稿一覧ページへリダイレクト。
+  end
+
+
+  private
+  def customer_params
+    params.require(:customers).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "/home/about" => "homes#about"
 
-  resources :user, only: [:show, :edit]
+  resources :user, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
