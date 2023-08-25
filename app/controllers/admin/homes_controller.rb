9a5e5445a2cddfb91de92d526413274369b4d00e
EC2 Default User <ec2-user@ip-172-31-43-94.ap-northeast-1.compute.internal>
2023-08-25 08:03:48 +0000

    注文履歴一覧（管理者）を作成中

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,6 @@ class Admin::HomesController < ApplicationController
 
   def top #注文履歴一覧
     @orders = OrderHistory.all
+    @details = OrderDetail.all
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ECsite
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo' #タイムゾーンを日本時間に設定
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
