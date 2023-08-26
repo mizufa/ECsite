0e86f93dc170355e57ec234053aa3ee84b483e08
EC2 Default User <ec2-user@ip-172-31-43-94.ap-northeast-1.compute.internal>
2023-08-26 19:38:06 +0000

    注文履歴詳細（管理者）完成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,7 @@ class Admin::OrdersController < ApplicationController
   def show #注文履歴詳細
     @order = OrderHistory.find(params[:id])
     @order_details = OrderHistory.all
+    @postage = 800 #送料
   end
 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,8 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item #商品に属する
+
+      ## 小計を求めるメソッド
+  def subtotal
+    item.with_tax_purchase_price * amount
+  end
 end
